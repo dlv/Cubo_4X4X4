75fb29d8b91dbb9f4c2a46149be4ac687fa32f49
Douglas Prado <douglas.prado@logicsp.com.br>
2017-08-15 14:02:31 -0300

    Adicionado função de Sequencia

--- test_cubo_4x4x4.ino
+++ test_cubo_4x4x4.ino
@@ -6,7 +6,6 @@ void apaga(int);
 
 void seq_1(int [], boolean);
 
-
 int RandFila;     //Variable para fila aleatoria
 int RandColumna;  //Variable para columna aleatoria
 	 
@@ -27,7 +26,8 @@ void loop()
 {
 //  RandLed();
 //  delay(150);
-  Sequencia();
+//  Sequencia();
+  Minhoca();
 }
 	 
 void RandLed()
@@ -45,6 +45,7 @@ void RandLed()
   	 
   delay(75);
 }
+
 
 void acendeFila(int posicao){
   digitalWrite(posicao, HIGH); 
@@ -66,91 +67,78 @@ void apaga(int posicao){
 
 void seq_1(int fila[], boolean acender){
   if (acender == true){
-    for (int i = 0; i < 5; i++) {
-    	acende(fila[i]);
+    for(int i = 0; i < 16; i++){
+        acende(fila[i]);
     }
   } else {
-     for (int i = 0; i < 5; i++) {
-    	apaga(fila[i]); 
+    for(int i = 15; i >= 0; i--){
+        apaga(fila[i]);
     }
   }
-    delay(80);
+//   delay(80);
 }
 
 
 void Sequencia()
 {
-  int dl = 80;
+  int dl = 150;
 
-  acendeFila(Filas[0]);
   int fila1[] = {1, 0, 14, 15};     // Fila 1
   int fila2[] = {2, 3, 4, 5};       // Fila 2
   int fila3[] = {9, 8, 7, 6};       // Fila 3
   int fila4[] = {10, 11, 12, 13};   // Fila 4 
- 
+
+  acendeFila(Filas[0]); 
+
   seq_1(fila1,true);
   seq_1(fila1,false);
-
   delay(dl);
 
   seq_1(fila2,true);
   seq_1(fila2,false);
-
   delay(dl);
 
-//  seq_1(fila3,true);
-//  seq_1(fila3,false);
-//
-//  delay(dl);
-//
-//  seq_1(fila4,true);
-//  seq_1(fila4,false);
+  seq_1(fila3,true);
+  seq_1(fila3,false);
+  delay(dl);
 
-  /********************************/
-  // Fila 2  
-//  acende(Columnas[2]);
-//  acende(Columnas[3]);
-//  acende(Columnas[4]);
-//  acende(Columnas[5]);
-//  
-//  delay(dl);
-//
-//  apaga(Columnas[2]);  
-//  apaga(Columnas[3]);
-//  apaga(Columnas[4]);
-//  apaga(Columnas[5]);
-//  
-//  delay(dl);
-  /********************************/
-  // Fila 3
-  acende(Columnas[9]);
-  acende(Columnas[8]);
-  acende(Columnas[7]);
-  acende(Columnas[6]);
-  
+  seq_1(fila4,true);
+  seq_1(fila4,false);
   delay(dl);
-  
-  apaga(Columnas[9]);  
-  apaga(Columnas[8]);
-  apaga(Columnas[7]);
-  apaga(Columnas[6]);
-  
-  delay(dl);
-  
-  /********************************/
-  // Fila 4
-  acende(Columnas[10]);
-  acende(Columnas[11]);
-  acende(Columnas[12]);
-  acende(Columnas[13]);
-
-  delay(dl);
-  
-  apaga(Columnas[10]);  
-  apaga(Columnas[11]);
-  apaga(Columnas[12]);
-  apaga(Columnas[13]);
 
   apagaFila(Filas[0]);
+  delay(150);
+}
+
+void Minhoca()
+{
+  int dl = 150;
+  
+  int seq1[] = {1,0,14,15,2,3,4,5,9,8,7,6,10,11,12,13};
+        
+  acendeFila(Filas[0]);
+  seq_1(seq1,true);
+  delay(dl);
+  seq_1(seq1,false);
+  apagaFila(Filas[0]);
+  
+  acendeFila(Filas[1]);
+  seq_1(seq1,true);
+  delay(dl);
+  seq_1(seq1,false);
+  apagaFila(Filas[1]);
+  
+  acendeFila(Filas[2]);
+  seq_1(seq1,true);
+  delay(dl);
+  seq_1(seq1,false);
+  apagaFila(Filas[2]);
+  
+  acendeFila(Filas[3]);
+  seq_1(seq1,true);
+  delay(dl);
+  seq_1(seq1,false);
+  apagaFila(Filas[3]);
+
   delay(150);
 }
